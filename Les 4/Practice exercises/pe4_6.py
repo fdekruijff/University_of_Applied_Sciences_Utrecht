a7d5685542cc9a18c34229b92609ee9b61f9c915
Floris de Kruijff <fdekruijff@users.noreply.github.com>
2017-09-13 18:04:02 +0200

    Modify pe4_6.py

--- Les 4/Practice exercises/pe4_6.py
+++ Les 4/Practice exercises/pe4_6.py
@@ -7,11 +7,25 @@
 letters = ['a', 'b', 'c']
 
 
-def modify(letters: list):
-    nonlocal letters
-    letters = ['d', 'e', 'f']
+def modify(old_letters: list):
+    old_letters.clear()
+    new_letters = ['d', 'e', 'f']
+
+    global letters
+    letters = new_letters
+
+
+def modify_improved():
+    return ['d', 'e', 'f']
 
 
 print(letters)
 modify(letters)
+letters = modify_improved()
 print(letters)
+
+
+# Je kan niet zo maar letters = list() doen omdat letters in de buitenste scope zit.
+# Op de manier waar ik het doe zou het ook werken met een String.
+# Globals gebruiken is bad-practice, het is beter om return statements te gebruiken
+# Een String object is inmutable. Een list kan gewijzigd worden.
