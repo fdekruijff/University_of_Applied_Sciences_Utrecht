0e4f105006c84b589973e17581eae8dccd9e73a8
Floris de Kruijff <fdekruijff@users.noreply.github.com>
2017-09-18 10:19:22 +0200

    Update pe3_2 / pe3_6

--- Les 3/Practice exercises/pe3_2.py
+++ Les 3/Practice exercises/pe3_2.py
@@ -6,21 +6,17 @@
 
 while True:
     try:
-        leeftijd = float(input("Geef je leeftijd: "))
+        leeftijd = int(input("Geef je leeftijd: "))
         passpoort = str(input("Nederlands paspoort: "))
 
-        if passpoort.lower() == 'ja' or passpoort.lower() == 'nee':
-            if leeftijd >= 18 and passpoort.lower() == 'ja':
-                print("Gefeliciteerd, je mag stemmen!")
-            elif leeftijd < 18:
-                print("Je bent nog te jong om te stemmen.")
-            else:
-                print("Je hebt een Nederlands passpoort nodig om te stemmen.")
-        else:
-            print("Geef 'Ja' of 'Nee' aan of u in bezit bent van een Nederlands paspoort.")
-            continue
+        if passpoort.lower() == 'ja' and (leeftijd >= 18):
+            print("Gefeliciteerd, je mag stemmen!")
+        elif passpoort.lower() == 'ja' and (leeftijd < 18):
+            print("Je bent nog te jong om te stemmen.")
+        elif passpoort.lower() == 'nee':
+            print("Je hebt een Nederlands passpoort nodig om te stemmen.")
 
         exit(0)
     except ValueError:
-        print("Voer een cijfer in.")
+        print("Incorrecte invoer, probeer opnieuw.")
         continue
--- Les 3/Practice exercises/pe3_6.py
+++ Les 3/Practice exercises/pe3_6.py
@@ -7,9 +7,5 @@
 s = "Guido van Rossum heeft programmeertaal Python bedacht."
 
 for letter in s:
-    if (str(letter).lower() == 'a' or
-        str(letter).lower() == 'e' or
-        str(letter).lower() == 'i' or
-        str(letter).lower() == 'o' or
-            str(letter).lower() == 'u'):
+    if letter in 'aeoui':
         print(letter)
