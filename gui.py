5d5640e63ddef1395e08be87f4aae4f54632f44a
Floris de Kruijff <fdekruijff@users.noreply.github.com>
2017-10-26 17:37:00 +0200

    working

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@
 from functools import *
 from tkinter import *
 from ClientNode import ClientNode
-from playsound import *
+# from playsound import *
 from math import *
 
 root = Tk()
@@ -28,15 +28,13 @@ client_list = [
     ClientNode('102.168.42.7', 5555, 'test1', None),
 ]
 
-
 def buttonClick():
     global staataan
     staataan = False if staataan else True
     print(staataan)
-
     if staataan:
         button.configure(bg = "red")
-        button.configure(text= "      Server Disabled      ")
+        button.configure(text= "        Server Disabled         ")
     else:
         button.configure(bg = "green")
         button.configure(text= "      Alarm Set     ")
@@ -89,12 +87,6 @@ def get_client_specifics(x):
         informationContainer.tempLabel.configure(text=client_info[label])
         rely += 0.085
 
-    if button.bg == "green":
-        root.button.configure(bg = "red")
-    else:
-        root.button.configure(bg = "green")
-
-
 root.informationContainer = Frame()
 root.mechanicListBox = Listbox()
 root.mechanicListBox.place(relx=0.020, rely=0.15, relwidth=0.45, relheight=0.80)
@@ -110,26 +102,12 @@ for client in client_list:
     root.mechanicListBox.insert(END, client.ip_address)
 
 button = Button(root, text = "      Alarm Set     " if staataan else "      Server Disabled      ", bg = "green" if
-root.mechanicListBox.configure(foreground="black")
-root.mechanicListBox.configure(relief=RAISED)
-root.mechanicListBox.configure(highlightbackground="black")
-root.mechanicListBox.configure(highlightcolor="black")
-root.mechanicListBox.configure(width=500)
-root.mechanicListBox.configure(height=100)
-root.mechanicListBox.bind("<Double-Button-1>")
-
-root.informationContainer.place(relx=0.520, rely=0.15, relwidth=0.45, relheight=0.80)
-root.informationContainer.configure(background="white")
-root.informationContainer.configure(highlightbackground="black")
-root.informationContainer.configure(highlightcolor="black")
-
-
-button = Button(root, text = "      Alarm Set     " if staataan else "      Alarm Disabled      ", bg = "green" if staataan else "red", command = buttonClick)
+                staataan else "red", command = buttonClick)
 button.grid(row=0,column=0)
 button.config(font=("Courier", 11))
 
-label = Label(root, text = "Status Alarm:\n" + ("ATTENTIE: ER GAAT EEN ALARM AF" if serverbool else "GEEN BIJZONDERHEDEN"))
 
+label = Label(root, text = "Status Alarm:\n" + ("ATTENTIE: ER GAAT EEN ALARM AF" if serverbool else "GEEN BIJZONDERHEDEN"))
 label.grid(row=0,column=1,padx=300)
 label.config(font=("Courier", 11))
 
