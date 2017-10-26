41fbe002cd07b58ba13a89266dbbbc2254a90de4
Joepieler <be.campagne@live.nl>
2017-10-26 17:06:20 +0200

    Update GPIO and Sockets
    * gpio functions
    
    * buttons weer werkend

--- client.py
+++ client.py
@@ -9,9 +9,9 @@ import RPi.GPIO as GPIO
 HOST = '192.168.42.2'
 PORT = 5555
 UUID = uuid.uuid4().hex
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#var GPIO
 connected = False
 registered = False
 debug = True
@@ -51,6 +51,9 @@ GPIO.setup(LCD_D7, GPIO.OUT)  # DB7
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
 
 def lcd_init():
@@ -122,7 +125,7 @@ def lcd_string(message, line):
     for i in range(LCD_WIDTH):
         lcd_byte(ord(message[i]), LCD_CHR)
 
-
+        
 def led_on(pin):
     GPIO.output(pin, GPIO.HIGH)
 
@@ -139,11 +142,9 @@ def button(x):
 
 def alarm_system_on():
     global system_on
-    global LCD_text_1
     led_on(26)
     system_on = True
-    LCD_text_1 = " Alarm is system_on"
-    lcd_string()
+    lcd_string(' Sytem is on', LCD_LINE_1)
     time.sleep(0.2)
 
 
@@ -153,12 +154,17 @@ def alarm_system_off():
     led_off(26)
     led_off(19)
     system_on = False
-    LCD_text_1 = " Alarm is uit"
+    lcd_string(' Sytem is off', LCD_LINE_1)
     time.sleep(0.5)
 
 
 def alarm_trigger():
     led_on(19)
+    alarm_tripped = True
+
+def alarm_trigger_off():
+    led_off(19)
+    alarm_tripped = False
 
 
 def gpio_mainloop():
@@ -168,51 +174,16 @@ def gpio_mainloop():
     global LCD_text_2
 
     while True:
-        if alarm_running:
-            GPIO.setup(26, GPIO.OUT)
-            GPIO.setup(19, GPIO.OUT)
-            GPIO.setup(13, GPIO.OUT)
-
-            alarm_running = False
-
-        if button(6) and not system_on:
+        if button(6) and system_on == False:
             alarm_system_on()
-        elif button(6) and system_on:
+
+
+        if button(6) and system_on:
             alarm_system_off()
 
-        if system_on and button(5):
+        if button(11) or button(5) and system_on:
             alarm_trigger()
-
-        # Send some test
-        lcd_string(LCD_text_1 + u' \u005C', LCD_LINE_1)
-        lcd_string(LCD_text_2, LCD_LINE_2)
-        # time.sleep(3)  # 3 second delay
-        alarm_system_on()
-        alarm_system_off()
-        alarm_trigger()
-
-        # Send some text
-        # lcd_string(LCD_text_1 + ' |', LCD_LINE_1)
-        lcd_string(LCD_text_2, LCD_LINE_2)
-
-        # time.sleep(3)  # 3 second delay
-        alarm_system_on()
-        alarm_system_off()
-        alarm_trigger()
-
-        # Send some text
-        # lcd_string(LCD_text_1+' /', LCD_LINE_1)
-        lcd_string(LCD_text_2, LCD_LINE_2)
-        #
-        # time.sleep(3)
-        alarm_system_on()
-        alarm_system_off()
-        alarm_trigger()
-
-        # Send some text
-        # lcd_string(LCD_text_1 + ' -', LCD_LINE_1)
-        lcd_string(LCD_text_2, LCD_LINE_2)
-
+            
 
 def get_time():
     return datetime.datetime.now().strftime('%d-%m-%Y %X')
@@ -231,18 +202,19 @@ def parse_socket_data(data: str):
     if data == "REG_COMPLETE":
         registered = True
     elif data == "ALRM_TRIP":
-        pass
+        alarm_trigger()
     elif data == "ALRM_STOP":
-        pass
+        alarm_trigger_off()
     elif data == "ALRM_ON":
-        pass
+        alarm_system_on()
     elif data == "ALRM_OFF":
-        pass
+        alarm_system_off()
     elif data == "CHNG_INTERVAL":
         pass
     elif data == "IS_ALIVE":
         socket_write("ACK", "IS_ALIVE")
         last_ping = time.time()
+        lcd_string(' Connected', LCD_LINE_2)
     elif data == "UUID_REQ":
         socket_write(str(UUID), "UUID")
     elif data == "STATUS_UPD":
@@ -257,8 +229,14 @@ def socket_write(data: str, data_header: str):
     """
     message = str(UUID) + "," + data_header + "," + data
     if debug: print("{} - Client send: {}".format(get_time(), message))
+      
+    try:
+        client_socket.send(message.encode('ascii'))
+    except ConnectionResetError or ConnectionAbortedError:
+        if debug: print("{} - Connection has been terminated by the server.".format(get_time()))
+        lcd_string(' Not Connected', LCD_LINE_2)
+        exit()
     client_socket.send(message.encode('ascii'))
-
 
 def socket_read():
     data = None
@@ -284,7 +262,10 @@ if __name__ == '__main__':
         finally:
             if debug: print("{} - Successfully connect to IP:{}, PORT:{}".format(get_time(), HOST, PORT))
 
-        # start_new_thread(gpio_mainloop, ())
+        lcd_init()
+        lcd_string(' Sytem is off', LCD_LINE_1)
+        lcd_string(' Connected', LCD_LINE_2)
+        start_new_thread(gpio_mainloop, ())
         start_new_thread(has_timeout, ())
 
         while True:
