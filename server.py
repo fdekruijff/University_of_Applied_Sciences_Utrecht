94b5a18f455ab6ffa305cfe5dbeaf7929a197ba6
Floris de Kruijff <fdekruijff@users.noreply.github.com>
2017-10-26 12:00:52 +0200

    server.py working sockets

--- server.py
+++ server.py
@@ -1,47 +1,41 @@
+import datetime
 import socket
 import time
 from _thread import *
+
 from ClientNode import ClientNode
 
 HOST = ''
 PORT = 5555
 client_list = []
+debug = True
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-def parse_data(client_hex, data_header, data):
+def get_time():
+    return datetime.datetime.now().strftime('%d-%m-%Y %X')
+
+
+def find_client(client_uuid: str) -> ClientNode:
+    for x in client_list:
+        if client_uuid == x.uuid:
+            return x
+
+
+def parse_socket_data(client_uuid, data_header, data):
+    client = find_client(client_uuid)
     if data == "ALRM_TRIP":
-        pass
-    elif data == "IS_ALIVE":
-        socket_write("ACK", client_hex)
+        client.alarm_tripped = True
 
     if data_header == "UUID":
         return data
     elif data_header == "STATUS_UPD":
         pass
+    elif data_header == "IS_ALIVE" and data == "ACK":
+        client.online = True
 
 
-def socket_is_alive(client_uuid):
-    client = None
-    for x in client_list:
-        if client_uuid == client.uuid:
-            client = x
-
-    try:
-        while True:
-            socket_write("IS_ALIVE", client_uuid)
-            data = server_socket.recv(2048).decode('utf-8').strip().split(',')[2]
-            if data == "ACK":
-                client.online = True
-            else:
-                client.online = False
-                # TODO: do some stuff here
-            time.sleep(1)
-    finally:
-        server_socket.close()
-
-
-def socket_write(message: str, client_uuid):
+def socket_write(conn, message: str, client_uuid):
     """
         return[0] = Client node UUID
         return[1] = data
@@ -53,16 +47,36 @@ def socket_write(message: str, client_uuid):
         # Unicast
         message = str(client_uuid) + "," + message
 
-    server_socket.send(message.encode('ascii'))
+    if debug: print("{} - Server send: {}".format(get_time(), message))
+    try:
+        conn.send(message.encode('ascii'))
+    except ConnectionAbortedError:
+        client = find_client(client_uuid)
+        client.online = False
+        client.connection_handler.close()
+        client_list.remove(client)
+        if debug: print(
+            "{} - Client with UUID {} has lost connection and has been unregistered.".format(get_time(), client_uuid))
 
 
 def socket_read(connection):
-    try:
-        while True:
-            data = connection.recv(2048).decode('utf-8').strip().split(',')
-            return parse_data(data[0], data[1], data[2])
-    finally:
-        connection.close()
+    while True:
+        data = connection.recv(2048).decode('utf-8').strip().split(',')
+        if debug: print("{} - Server received: {}".format(get_time(), data))
+        parse_socket_data(data[0], data[1], data[2])
+
+
+def get_uuid(connection):
+    socket_write(connection, "UUID_REQ", None)
+    data = connection.recv(2048).decode('utf-8').strip().split(',')
+    return parse_socket_data(data[0], data[1], data[2])
+
+
+def clients_alive():
+    while True:
+        for client in client_list:
+            socket_write(client.connection_handler, "IS_ALIVE", client.uuid)
+        time.sleep(2.5)
 
 
 if __name__ == '__main__':
@@ -70,17 +84,18 @@ if __name__ == '__main__':
         server_socket.bind((HOST, PORT))
         server_socket.listen(2)
     except socket.error as e:
-        print("Socket error {}".format(e))
+        if debug: print("{} - Socket error {}".format(get_time(), e))
         exit()
     finally:
-        print("Successfully connect to IP:{}, PORT:{}".format(HOST, PORT))
+        if debug: print("{} - Successfully bound to socket, PORT:{}".format(get_time(), PORT))
 
     while True:
         c, i = server_socket.accept()
 
-        socket_write("UUID_REQ", None)
-        uuid = socket_read(c)
+        uuid = get_uuid(c)
         client_list.append(ClientNode(i[0], i[1], uuid, c))
-        socket_write("REG_COMPLETE", uuid)
+        socket_write(c, "REG_COMPLETE", uuid)
+        if debug: print("{} - Client with UUID: {}, connected successfully".format(get_time(), uuid))
 
         start_new_thread(socket_read, (c,))
+        start_new_thread(clients_alive, ())
