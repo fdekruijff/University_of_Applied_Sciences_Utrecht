ccb7f4181f3eb4a0b6b060d4b753f5c208fa22dc
Joepieler <be.campagne@live.nl>
2017-10-28 11:08:18 +0200

    Joepieler final update working client.py
    * gpio functions
    
    * buttons weer werkend
    
    * buttens werkend
    
    * final alles werkend

--- client.py
+++ client.py
@@ -2,11 +2,13 @@ import datetime
 import socket
 import time
 import uuid
+import pygame
+import sys
 from _thread import *
 
 import RPi.GPIO as GPIO
 
-HOST = '192.168.42.2'
+HOST = '145.89.96.103'
 PORT = 5555
 UUID = uuid.uuid4().hex
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,7 +24,14 @@ alarm_enabled = False
 alarm_interval_seconds = 5
 alarm_running = True
 system_on = False
+alarm_trigger_time = 0
 alarm_on = False
+trigger_delay = 5
+server_disable_alarm = False
+
+pygame.mixer.init()
+pygame.mixer.music.load("shootingstars.mp3")
+
 
 # LCD var declarations
 LCD_RS = 7
@@ -146,6 +155,7 @@ def alarm_system_on():
     global LCD_text_1
     led_on(26)
     system_on = True
+    socket_write("", "ALRM_ON")
     LCD_text_1 =' Sytem is on'
     time.sleep(0.2)
 
@@ -155,24 +165,62 @@ def alarm_system_off():
     global LCD_text_1
     led_off(26)
     led_off(19)
+    led_off(13)
     system_on = False
+    socket_write("", "ALRM_OFF")
     LCD_text_1 = ' Sytem is off'
     time.sleep(0.5)
+    pygame.mixer.music.stop()
+
+
+def flikker(x):
+    while True:
+        led_on(x)
+        time.sleep(0.2)
+        led_off(x)
+        time.sleep(0.2)
+        if system_on == False:
+
+            led_off(x)
+            break
 
 
 def alarm_trigger():
-    led_on(19)
+    global server_disable_alarm
     alarm_tripped = True
+    led_on(13)
+    while system_on:
+        if button(6):
+            break
+        if server_disable_alarm:
+            server_disable_alarm = False
+            break
+        global alarm_tripped
+        if time.time() - alarm_trigger_time >= trigger_delay and alarm_trigger_time != 0:
+            pygame.mixer.music.play()
+            socket_write("", "ALRM_TRIP")
+            time.sleep(0.5)
+            led_off(13)
+            start_new_thread(flikker(19), ())
+            print(alarm_tripped)
+            break
 
 
 def alarm_trigger_off():
+    global server_disable_alarm
+    global alarm_tripped
     led_off(19)
+    server_disable_alarm = True
     alarm_tripped = False
+    led_off(19)
+    time.sleep(0.5)
+    pygame.mixer.music.stop()
 
 
 def gpio_mainloop():
     global alarm_running
     global system_on
+    global alarm_trigger_time
     global LCD_text_1
     global LCD_text_2
     GPIO.output(26, 0)
@@ -186,9 +234,14 @@ def gpio_mainloop():
         if button(6) and system_on:
             alarm_system_off()
 
-        if button(11) or button(5) and system_on:
-            alarm_trigger()
+        if button(11) and system_on == True:
+            alarm_trigger_time = time.time()
+            start_new_thread (alarm_trigger, ())
+            print (system_on)
 
+        if button(5) and system_on == True:
+            alarm_trigger_time = time.time()
+            alarm_trigger()
         lcd_string(LCD_text_1, LCD_LINE_1)
         lcd_string(LCD_text_2, LCD_LINE_2)
 
@@ -212,8 +265,16 @@ def has_timeout():
 def parse_socket_data(data: str):
     global registered
     global last_ping
+    global server_disable_alarm
     if data == "REG_COMPLETE":
         registered = True
+    elif data == "ALRM_CHNG":
+        if alarm_tripped:
+            alarm_trigger_off()
+        else:
+            alarm_trigger()
+    elif data == "ALRM_STATUS":
+        pass
     elif data == "ALRM_TRIP":
         alarm_trigger()
     elif data == "ALRM_STOP":
@@ -221,6 +282,7 @@ def parse_socket_data(data: str):
     elif data == "ALRM_ON":
         alarm_system_on()
     elif data == "ALRM_OFF":
+        server_disable_alarm = True
         alarm_system_off()
     elif data == "CHNG_INTERVAL":
         pass
@@ -241,6 +303,7 @@ def socket_write(data: str, data_header: str):
         return[1] = data_header
         return[2] = data
     """
+    global LCD_text_2
     message = str(UUID) + "," + data_header + "," + data
     if debug: print("{} - Client send: {}".format(get_time(), message))
 
@@ -248,7 +311,6 @@ def socket_write(data: str, data_header: str):
         client_socket.send(message.encode('ascii'))
     except ConnectionResetError or ConnectionAbortedError:
         if debug: print("{} - Connection has been terminated by the server.".format(get_time()))
-        global LCD_text_2
         LCD_text_2 = ' Not Connected'
         exit()
     client_socket.send(message.encode('ascii'))
@@ -262,6 +324,9 @@ def socket_read():
         if debug: print("{} - Connection has been terminated by the server.".format(get_time()))
         exit()
     data = data.decode('utf-8').strip().split(',')
+    if data[0] == '':
+
+        alarm_trigger()
     if debug: print("{} - Client received: {}".format(get_time(), data))
     if (data[0] == UUID) or (data[0] == "BROADCAST"):
         return parse_socket_data(data[1])
