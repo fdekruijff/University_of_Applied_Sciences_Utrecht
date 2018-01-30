4dc24c5aa70d8fc7f670df7833784b3469f82d83
Floris de Kruijff <floris.dekruijff@gmail.com>
2018-01-30 11:07:11 +0100

    LCD code samengevoegd met server.py

--- Server.py
+++ Server.py
@@ -9,8 +9,10 @@ import json
 import socket
 import sys
 import time
-from _thread import *
 
+import RPi.GPIO as GPIO
+
+from _thread import *
 from Node import Node
 
 
@@ -25,14 +27,89 @@ class Server:
         self.barrier_open = False
         self.operational = True
 
+        # Define GPIO to LCD mapping
+        self.LCD_RS = 7
+        self.LCD_E = 8
+        self.LCD_D4 = 25
+        self.LCD_D5 = 24
+        self.LCD_D6 = 23
+        self.LCD_D7 = 18
+
+        # Define some device constants
+        self.LCD_WIDTH = 16  # Maximum characters per line
+        self.LCD_CHR = True
+        self.LCD_CMD = False
+
+        self.LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
+        self.LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+
+        # Timing constants
+        self.E_PULSE = 0.0005
+        self.E_DELAY = 0.0005
+
     @staticmethod
     def get_time() -> str:
         """ Returns current time in format %d-%m-%Y %X """
         return datetime.datetime.now().strftime('%d-%m-%Y %X')
 
-    def change_barrier(self):
-        """ Open or close barrier based on variable """
-        pass
+    def lcd_main(self):
+        try:
+            # Main program block
+            GPIO.setwarnings(False)
+            GPIO.setmode(GPIO.BCM)  # Use BCM GPIO numbers
+            GPIO.setup(self.LCD_E, GPIO.OUT)  # E
+            GPIO.setup(self.LCD_RS, GPIO.OUT)  # RS
+            GPIO.setup(self.LCD_D4, GPIO.OUT)  # DB4
+            GPIO.setup(self.LCD_D5, GPIO.OUT)  # DB5
+            GPIO.setup(self.LCD_D6, GPIO.OUT)  # DB6
+            GPIO.setup(self.LCD_D7, GPIO.OUT)  # DB7
+            GPIO.setup(21, GPIO.IN)  # schakelaar
+            GPIO.setup(20, GPIO.IN)  # schakelaar
+            GPIO.setup(16, GPIO.IN)  # raspberry 1
+            GPIO.setup(12, GPIO.IN)  # raspberry 2
+
+            # Initialise display
+            self.lcd_init()
+
+            while True:
+                # status raspberry
+                if self.switch_status() == 0:
+                    time.sleep(0.5)
+                    if self.switch_status() == 0:
+                        while True:
+                            self.lcd_string(self.status_rasbberry(1), self.LCD_LINE_1)
+                            self.lcd_string(self.status_rasbberry(0), self.LCD_LINE_2)
+                            if self.switch_status() != 0:
+                                time.sleep(0.5)
+                                if self.switch_status() != 0:
+                                    break
+
+                # status pijl
+                elif self.switch_status() == 1:
+                    time.sleep(0.5)
+                    if self.switch_status() == 1:
+                        while True:
+                            self.lcd_string("Water niveau", self.LCD_LINE_1)
+                            self.lcd_string(10, self.LCD_LINE_2) # TODO: fixen
+                            if self.switch_status() != 1:
+                                time.sleep(0.5)
+                                if self.switch_status() != 1:
+                                    break
+                elif self.switch_status() == 2:
+                    time.sleep(0.5)
+                    if self.switch_status() == 2:
+                        self.lcd_string("Kering: open", self.LCD_LINE_1) # TODo: fixen
+                        self.lcd_string("", self.LCD_LINE_2)
+                        if self.switch_status() != 2:
+                            time.sleep(0.5)
+                            if self.switch_status() != 2:
+                                break
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.lcd_byte(0x01, self.LCD_CMD)
+            self.lcd_string("Goodbye!", self.LCD_LINE_1)
+            GPIO.cleanup()
 
     def init_socket(self) -> None:
         """ Initialises server socket """
@@ -156,6 +233,102 @@ class Server:
                 except ConnectionError or ConnectionResetError:
                     self.remove_client(uuid)
             time.sleep(2.5)
+
+    def lcd_kering_status(self) -> str:
+        if self.barrier_open:
+            return "kering is dicht"
+        else:
+            return "kering is open"
+
+    def status_rasbberry(self, x) -> str:
+        # TODO: Moet werken met sockets en niet met GPIO
+        if x:
+            input_value = GPIO.input(16)
+            if input_value:
+                return 'PI 1  actief'
+            return 'PI 1 Inactief'
+        elif not x:
+            input_value = GPIO.input(12)
+            if input_value:
+                return 'PI 2  actief'
+            return 'PI 2 Inactief'
+
+    @staticmethod
+    def switch_status() -> int:
+        if GPIO.input(21):
+            return 0
+        elif GPIO.input(20):
+            return 1
+        else:
+            return 2
+
+    def lcd_init(self):
+        # Initialise display
+        self.lcd_byte(0x33, self.LCD_CMD)  # 110011 Initialise
+        self.lcd_byte(0x32, self.LCD_CMD)  # 110010 Initialise
+        self.lcd_byte(0x06, self.LCD_CMD)  # 000110 Cursor move direction
+        self.lcd_byte(0x0C, self.LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
+        self.lcd_byte(0x28, self.LCD_CMD)  # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01, self.LCD_CMD)  # 000001 Clear display
+        time.sleep(self.E_DELAY)
+
+    def lcd_byte(self, bits, mode):
+        # Send byte to data pins
+        # bits = data
+        # mode = True  for character
+        #        False for command
+
+        GPIO.output(self.LCD_RS, mode)  # RS
+
+        # High bits
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits & 0x10 == 0x10:
+            GPIO.output(self.LCD_D4, True)
+        if bits & 0x20 == 0x20:
+            GPIO.output(self.LCD_D5, True)
+        if bits & 0x40 == 0x40:
+            GPIO.output(self.LCD_D6, True)
+        if bits & 0x80 == 0x80:
+            GPIO.output(self.LCD_D7, True)
+
+        # Toggle 'Enable' pin
+        self.lcd_toggle_enable()
+
+        # Low bits
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits & 0x01 == 0x01:
+            GPIO.output(self.LCD_D4, True)
+        if bits & 0x02 == 0x02:
+            GPIO.output(self.LCD_D5, True)
+        if bits & 0x04 == 0x04:
+            GPIO.output(self.LCD_D6, True)
+        if bits & 0x08 == 0x08:
+            GPIO.output(self.LCD_D7, True)
+
+        # Toggle 'Enable' pin
+        self.lcd_toggle_enable()
+
+    def lcd_toggle_enable(self):
+        # Toggle enable
+        time.sleep(self.E_DELAY)
+        GPIO.output(self.LCD_E, True)
+        time.sleep(self.E_PULSE)
+        GPIO.output(self.LCD_E, False)
+        time.sleep(self.E_DELAY)
+
+    def lcd_string(self, message, line):
+        # Send string to display
+        message = message.ljust(self.LCD_WIDTH, " ")
+        self.lcd_byte(line, self.LCD_CMD)
+
+        for i in range(self.LCD_WIDTH):
+            self.lcd_byte(ord(message[i]),self. LCD_CHR)
 
 
 if __name__ == '__main__':
